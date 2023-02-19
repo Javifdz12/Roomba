0b5f318690915e1cd6c6827b64f876e19762ac70
Javifdz12 <101470100+Javifdz12@users.noreply.github.com>
2023-02-20 00:47:30 +0100

    GG

--- lanzador.py
+++ lanzador.py
@@ -2,44 +2,49 @@ from tkinter import Tk,LabelFrame,Text,Label
 from obstaculo import Obstaculo
 from prueba import Texto
 
-"""
+def añadir_obs(obss):
+        for i in obss:
+            i.colocar_ob()
+
 def lanzar():
     print('#####-ROOMBA-#####')
     z=float(input('¿Cuál es la velocidad de tu rumba(m2/h o cm/s)? '))
-    a=int(input('¿Cuál es el largo de la habitacion(cm)? '))
-    s=int(input('¿Cuál es el ancho de la habitacion(cm)? '))
-    a1=a*s
+    a=int(input('¿Cuál es el ancho de la habitacion(cm)? '))
+    s=int(input('¿Cuál es el largo de la habitacion(cm)? '))
     mi_ventana = Tk()
-    mi_ventana.geometry(f'{a}x{s}')
     mi_ventana.title('Habitación')
+    marco=LabelFrame(mi_ventana,width=a,height=s)
+    marco.pack(padx=30,pady=30)
+    a1=(a-30)*(s-30)
     b=int(input('¿Cuántos obstáculos tiene la habitacion? '))
     print('Si la habitación es la siguiente:')
-    print(f'{a}')
+    print(f'{s}')
     print('  ^')
     print('y |\n')
     print('  |\n')
     print('  |\n')
     print('  |\n')
     print('  |')
-    print(f'   -- -- -- -- -- -- --  > {s}\n                      x')
+    print(f'   -- -- -- -- -- -- --  > {a}\n                       x')
     colores=['blue','yellow','red']
     a2=0
     i=0
+    obss=[]
     while i<b:
-        c=float(input(f'¿Cuál es la posicion x de la esquina superior derecha obstaculo {i+1}? '))
-        d=float(input(f'¿Cuál es la posicion y de la esquina superior derecha obstaculo {i+1}? '))
-        e=float(input(f'¿Cuál es el largo del obstaculo {i+1}?(cm) '))
-        f=float(input(f'¿Cuál es el ancho del obstaculo {i+1}?(cm) '))
-        obs=Obstaculo(c,d,e,f,mi_ventana,bg='black')
-        obs.colocar_ob()
+        c=int(input(f'¿Cuál es la posicion x de la esquina superior derecha obstaculo {i+1}? '))
+        d=int(input(f'¿Cuál es la posicion y de la esquina superior derecha obstaculo {i+1}? '))
+        e=int(input(f'¿Cuál es el largo del obstaculo {i+1}?(cm) '))
+        f=int(input(f'¿Cuál es el ancho del obstaculo {i+1}?(cm) '))
+        obss.append(Obstaculo(c,d,e,f,marco,bg='grey',relief='sunken'))
         a2+=(e*f)
         i+=1
+    añadir_obs(obss)
     mi_ventana.mainloop()
     af=a1-a2
     print(f'El area a limpiar es {af/10000} m2')
     print(f'Como la velocidad de la roomba es de {z} m2/h...')
     print(f'El tiempo de limpieza será de {tiempo_limp((af/10000),z)} minutos')
-"""
+
 
 #Funcion para calcular tiempo de limpieza.
 def tiempo_limp(area_hab,vel_rob):
@@ -48,7 +53,7 @@ def tiempo_limp(area_hab,vel_rob):
     t=area_hab/vel_rob
     return t*60
 
-def lanzar():
+def lanzar2():
     #Esta seria la ventana principal del ejercicio.
     ventana=Tk()
     ventana.title('Habitación')
